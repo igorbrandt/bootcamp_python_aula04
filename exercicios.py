f02188f1f090ac2b367a2f2db1e85be28b8216e0
unknown <igorbrandt95@hotmail.com>
2025-06-11 14:37:41 -0300

    :wrench: incluí # %% do jupiter notebook para rodar cada exertcício individualmente

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,5 @@
 ## Exercícios de Listas e Dicionários
+# %%
 ### 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 lista = []
 for i in range(1, 11):
@@ -6,13 +7,14 @@ for i in range(1, 11):
 
 print(lista)
 
-
+# %%
 ### 2. Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
 lista: list = ["Python", "Java", "C++", "JavaScript"]
 lista.remove("C++")
 lista.append("Ruby")
 print(lista)
 
+# %%
 ### 3. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
 # Hipótese com diferentes dicionários, que podem ser incluídos em uma lista:
 livro_1: dict = {
@@ -53,6 +55,7 @@ listar_livros()
 print(lista_de_livros_dict["livro_1"])
 print(lista_de_livros_dict["livro_1"]["titulo"])
 
+# %%
 ### 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 def contar_caracteres(s):
     contagem = {}
@@ -63,6 +66,7 @@ def contar_caracteres(s):
 frase = "Comprei um limão e um abcaaxi.."
 print(contar_caracteres(frase))
 
+# %%
 ### 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
 frutas: list = ["maçã", "banana", "cereja"]
 precos_frutas: dict = {"maçã": 0.45, 
@@ -74,6 +78,7 @@ for fruta in precos_frutas:
 
 print(valor_total)
 
+# %%
 ### 6 Eliminação de Duplicatas. Objetivo: Dada uma lista de emails, remover todos os duplicados.
 lista_emails = ["pessoa1@email.com", "pessoa1@email.com", "pessoa2@email.com", "pessoa3@email.com"]
 
@@ -89,6 +94,7 @@ def remover_emails_duplicados():
 
 remover_emails_duplicados()
 
+# %%
 ### 7. Filtragem de Dados. Objetivo: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
 lista_idades = [5, 6, 8, 18, 21, 44, 55, 9]
 
@@ -103,16 +109,19 @@ def listar_maiores_de_18():
 
 listar_maiores_de_18()
 
+# %%
 ### 8. Ordenação Personalizada. Objetivo: Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
 lista_de_pessoas: list = [{"nome": "igor", "idade": 29},{"nome": "enzo", "idade":26},{"nome":"pedro", "idade":63}]
 lista_de_pessoas_ordenada = sorted(lista_de_pessoas, key=lambda x: x["nome"])
 print(lista_de_pessoas_ordenada)
 
+# %%
 ### 9. Agregação de Dados. Objetivo: Dado um conjunto de números, calcular a média.
 numeros = [1, 8]
 media = sum(numeros) / len(numeros)
 print(media)
 
+# %%
 ### 10. Divisão de Dados em Grupos. Objetivo: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
 lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 0]
 numeros_pares = []
@@ -129,6 +138,7 @@ def identificar_numeros_pares_e_impares():
 
 identificar_numeros_pares_e_impares()
 
+# %%
 ### 11. Atualização de Dados. Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
 precos_produtos: dict = {"óleo": 2.39, 
                        "desodorante": 15.20, 
@@ -142,12 +152,14 @@ def atualizar_preco_do_produto():
 
 atualizar_preco_do_produto()
 
+# %%
 ### 12. Fusão de Dicionários. Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
 parametros_igor = {"igor": 29}
 parametros_enzo = {"enzo": 26}
 parametros_totais = parametros_igor | parametros_enzo
 print(parametros_totais)
 
+# %%
 ### 13. Filtragem de Dados em Dicionário. Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
 estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
 
@@ -160,6 +172,7 @@ def mostrar_produtos_com_quantidade_positiva():
 
 mostrar_produtos_com_quantidade_positiva()
 
+# %%
 ### 14. Extração de Chaves e Valores. Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
 dicionario = {"a": 1, "b": 2, "c": 3}
 chaves = list(dicionario.keys())
@@ -167,6 +180,7 @@ valores = list(dicionario.values())
 print(chaves)
 print(valores)
 
+# %%
 ### 15. Contagem de Frequência de Itens. Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
 string = "Cheguei da escola e fui jogar bola na pracinha"
 
@@ -182,6 +196,7 @@ def contar_numero_de_caracteres():
 
 contar_numero_de_caracteres()
 
+# %%
 ### 16. Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
 def somar_numeros():
     somatorio_total = 0
@@ -194,6 +209,7 @@ def somar_numeros():
 
 somar_numeros()
 
+# %%
 ### 17. Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
 def eh_numero_primo(numero):
     if numero < 2: # Números menores que 2 não são primos
@@ -209,6 +225,7 @@ if eh_numero_primo(num):
 else:
     print(f"{num} não é um número primo. ")
 
+# %%
 ### 18. Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
 def reverter_string_de_texto(string):
     string_revertida = ''.join(reversed(string))
@@ -217,6 +234,7 @@ def reverter_string_de_texto(string):
 texto = input("Digite um texto para ser revertido: ")
 print(reverter_string_de_texto(texto))
 
+# %%
 ### 19. Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
 def encontrar_pares(lista, alvo):
     pares = []
@@ -234,6 +252,7 @@ numeros = [1, 2, 3, 4, 5, 6]
 alvo = 7
 print(encontrar_pares(numeros, alvo))
 
+# %%
 ### 20. Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
 dicionario: dict = {"nome":"igor","idade":29,"altura":1.74}
 
@@ -242,3 +261,5 @@ def listar_de_chaves(dicionario):
     return lista_de_chaves
     
 print(listar_de_chaves(dicionario))
+
+# %%
